ed0569e5dc81cae4c828fbd49e06f79365c46d8d
Nicolas Theau <theaun91@gmail.com>
2022-11-04 19:49:11 +0100

    Démarrage de slash + exit + format du prompt
    Co-authored-by: ZHOU sebastien <sebastien.4084@gmail.com>
    Co-authored-by: René Tom <re.tom2003@gmail.com>

--- Makefile
+++ Makefile
@@ -1,19 +1,19 @@
 CC ?= gcc
 CCFLAGS ?= -g -Wall -std=c99
 
-all: slash pwd
+all: slash
 
 slash: 
-	$(CC) $(CCFLAGS) -o bin/slash  -c src/slash.c
+	$(CC) $(CCFLAGS) -o bin/slash  src/slash.c
 
 # pwd: 
-# 	$(CC) $(CCFLAGS) -o bin/pwd  -c src/pwd.c
+# 	$(CC) $(CCFLAGS) -o bin/pwd  src/pwd.c
 
 # exit: 
-# 	$(CC) $(CCFLAGS) -o bin/exit  -c src/exit.c
+# 	$(CC) $(CCFLAGS) -o bin/exit  src/exit.c
 
 # cd: 
-# 	$(CC) $(CCFLAGS) -o bin/cd  -c src/cd.c
+# 	$(CC) $(CCFLAGS) -o bin/cd  src/cd.c
 
 clean:
 	rm -f bin/*
--- src/slash.c
+++ src/slash.c
@@ -1,6 +1,57 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+
 #define MAX_ARGS_NUMBER 4096
 #define MAX_ARGS_STRLEN 4096
+#define MAX_PATH 4096
+#define MAX_PRINTED_PATH 24
+
+char buffer[MAX_ARGS_STRLEN];
+int val_retour = 0;
+// char position[MAX_PATH];
+
+
+void promptFormat() {
+    char * position = "zzzzzzzzzzzzzzzzzzzzzzzzz";
+    int taille = 0;
+    if (val_retour == 0) {
+        printf("\033[32m[0]");
+        taille += 3;
+    } else {
+        printf("\033[91m[1]");
+        taille+= 3;
+    }
+    printf("\033[36m");
+    int taille_restant = 30 - taille - 2;
+    int taille_position = strlen(position);
+    if(taille_position > 25) {
+        taille_restant -= 3;
+        printf("...");
+    }
+    if(taille_position < taille_restant) {
+        taille_restant = taille_position;
+    }
+    printf("%.*s", taille_restant, position + taille_position - taille_restant);
+    printf("\033[00m$ ");
+}
 
 int main(int argc, char ** argv) {
+    if(argc > 1) {
+        printf("\033[91mTrop d'arguments \033[00m\n");
+        return 1;
+    }
+    promptFormat();
+    while(1) {
+        scanf("%s", buffer);
+        if(strcmp(buffer, "exit") == 0) {
+            return 0;
+        }
+        else {
+            val_retour = 1;
+            printf("Commande inconnue\n");
+            promptFormat();
+        }
+    }
     return 0;
 }
