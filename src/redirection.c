31b66de5293ad982161b271977e012e233cf7127
ZHOU <sebastien.4084@gmail.com>
2023-01-08 22:43:01 +0100

    factorisation

--- src/redirection.c
+++ src/redirection.c
@@ -19,12 +19,7 @@ int redirect(int * input_fd, int * output_fd, int * err_fd, commande * cmd){
                 return 1;
             }
             //Recréation de la commande avant la redirection
-                for (int j = i; j < cmd -> argc - 2; j++) {
-                    cmd -> args[j] = cmd -> args[j + 2];
-                }
-                cmd -> argc -= 2;
-                cmd -> args[cmd -> argc] = NULL;
-                i--;
+
 
         }else if (strcmp(cmd -> args[i], ">") == 0) {
             if (cmd -> args[i + 1] == NULL || cmd -> args[i + 1][0] == '|') {
@@ -36,64 +31,30 @@ int redirect(int * input_fd, int * output_fd, int * err_fd, commande * cmd){
                 perror("open");
                 return 1;
             }
-            for (int j = i; j < cmd -> argc - 2; j++) {
-                cmd -> args[j] = cmd -> args[j + 2];
-            }
-            cmd -> argc -= 2;
-            cmd -> args[cmd -> argc] = NULL;
-            i--;
-
         }else if (strcmp(cmd -> args[i], ">>") == 0) {
             *output_fd = open(cmd -> args[i + 1], O_WRONLY | O_CREAT | O_APPEND, 0664);
             if (*output_fd == -1) {
                 perror("open");
                 return 1;
             }
-            for (int j = i; j < cmd -> argc - 2; j++) {
-                cmd -> args[j] = cmd -> args[j + 2];
-            }
-            cmd -> argc -= 2;
-            cmd -> args[cmd -> argc] = NULL;
-            i--;
-
         }else if (strcmp(cmd -> args[i], ">|") == 0) {
             *output_fd = open(cmd -> args[i + 1], O_WRONLY | O_CREAT | O_TRUNC, 0664);
             if (*output_fd == -1) {
                 perror("open");
                 return 1;
             }
-            for (int j = i; j < cmd -> argc - 2; j++) {
-                cmd -> args[j] = cmd -> args[j + 2];
-            }
-            cmd -> argc -= 2;
-            cmd -> args[cmd -> argc] = NULL;
-            i--;
-
         }else if (strcmp(cmd -> args[i], "2>") == 0) {
             *err_fd = open(cmd -> args[i + 1], O_WRONLY | O_CREAT | O_TRUNC | O_EXCL, 0664);
             if (*err_fd == -1) {
                 perror("open");
                 return 1;
             }  
-            for (int j = i; j < cmd -> argc - 2; j++) {
-                cmd -> args[j] = cmd -> args[j + 2];
-            }
-            cmd -> argc -= 2;
-            cmd -> args[cmd -> argc] = NULL;
-            i--;
-
         }else if (strcmp(cmd -> args[i], "2>>") == 0) {
             *err_fd = open(cmd -> args[i + 1], O_WRONLY | O_CREAT | O_APPEND, 0664);
             if (*err_fd == -1) {
                 perror("open");
                 return 1;
             }
-            for (int j = i; j < cmd -> argc - 2; j++) {
-                cmd -> args[j] = cmd -> args[j + 2];
-            }
-            cmd -> argc -= 2;
-            cmd -> args[cmd -> argc] = NULL;
-            i--;
 
         }else if (strcmp(cmd -> args[i], "2>|") == 0) {
             *err_fd = open(cmd -> args[i + 1], O_WRONLY | O_CREAT | O_TRUNC, 0664);
@@ -101,13 +62,15 @@ int redirect(int * input_fd, int * output_fd, int * err_fd, commande * cmd){
                 perror("open");
                 return 1;
             }
-            for (int j = i; j < cmd -> argc - 2; j++) {
-                cmd -> args[j] = cmd -> args[j + 2];
-            }
-            cmd -> argc -= 2;
-            cmd -> args[cmd -> argc] = NULL;
-            i--;
+        } else {
+            continue;
         }
+        for (int j = i; j < cmd -> argc - 2; j++) {
+            cmd -> args[j] = cmd -> args[j + 2];
+        }
+        cmd -> argc -= 2;
+        cmd -> args[cmd -> argc] = NULL;
+        i--;
     }
     return 0;
 }
